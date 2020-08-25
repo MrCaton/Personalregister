48efbb76796879e3b0330e17c6be43218965eec0
MrCaton <70195862+MrCaton@users.noreply.github.com>
2020-08-25 15:57:08 +0200

    Add files via upload

---
+++ Personalregister/Personalregister/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Personalregister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How many employees do you want to add?");
+            int n= int.Parse(Console.ReadLine());
+            for (int i=0; i < n; i++)
+            { 
+                Console.WriteLine("Insert the name of your employee");
+                string name = Console.ReadLine();
+                Console.WriteLine("Insert the salary of your employee");
+                int salary = int.Parse(Console.ReadLine());
+                Console.WriteLine(name + " " + salary);
+            }
+
+        }
+    }
+}
